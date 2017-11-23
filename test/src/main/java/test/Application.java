d69170d7c2f0975fd28bd404eed169188895cc4b
mengbing <mengbing@yuntongxun.com>
2017-11-23 14:28:19 +0800

    init

---
+++ test/src/main/java/test/Application.java
@@ -0,0 +1,16 @@
+package test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class Application {
+    public static void main(String[] args) {
+    	
+    	System.out.println("start...");
+    	
+        SpringApplication.run(Application.class, args);
+    }
+}
